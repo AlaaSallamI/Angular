75e6f89c77fdd89ff0d1bd3629ef47bdaf08129b
Alaa Sallam <alaasallam567@gmail.com>
2019-10-20 17:25:01 +0200

    first commit menu

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 
+import 'hammerjs';
+import { MenueComponent } from './menue/menue.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenueComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    FlexLayoutModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
